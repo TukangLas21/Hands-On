345a8725515c8db5f69f22e2f5eda331cbdfef61
TukangLas21 <19623160@std.stei.itb.ac.id>
2024-08-09 23:09:33 +0700

    feat: initialized about, projects, and contact page

---
+++ app/contact.tsx
@@ -0,0 +1,9 @@
+export default function Contact() {
+    return (
+      <div>
+        <h1>Contact Me</h1>
+        <p>Email: </p>
+        <p>LinkedIn: <a href="">yourprofile</a></p>
+      </div>
+    );
+  }
---
+++ app/projects.tsx
@@ -0,0 +1,12 @@
+export default function Projects() {
+    return (
+      <div>
+        <h1>My Projects</h1>
+        <ul>
+          <li>Project 1: Description</li>
+          <li>Project 2: Description</li>
+          <li>Project 3: Description</li>
+        </ul>
+      </div>
+    );
+  }
