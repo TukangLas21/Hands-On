493a56744d58ff5e1c7dc4a62ca749b52ba5713c
TukangLas21 <19623160@std.stei.itb.ac.id>
2024-08-10 01:35:49 +0700

    feat: added more content in about page, refactor: removed unused CSS line

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,19 @@
 export default function About() {
     return (
-      <div>
-        <h1>About Me</h1>
-        <p>Brief introduction about yourself, your skills, and experience.</p>
+      <div className="hero">
+        <div className="about_text">
+          <h1>About Me</h1>
+          <p>
+            I am a second-year student currently enrolled in Bandung Institute of Technology, majoring in Informatics Engineering. <br>
+          </br>
+            I was born in Jakarta, at 25 September 2005, but I lived most of my life in BSD, South Tangerang.
+          </p>
+
+          <h2>Interests</h2>
+          <p></p>
+        </div>
+        
+        
       </div>
     );
   }
