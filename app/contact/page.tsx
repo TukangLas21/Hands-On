53a1a8bb7fcf016d17fe8293a93b3148e7542f0e
TukangLas21 <19623160@std.stei.itb.ac.id>
2024-08-10 22:19:48 +0700

    feat: added links to contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link';
+
 export default function Contact() {
     return (
       <div className="contact_text">
         <h1>Contact Me</h1>
-        <p>Email: </p>
-        <p>LinkedIn: <a href="">yourprofile</a></p>
+        <h2>Feel free to contact me for any inquiries in the following:</h2>
+        <p>Email: <Link href='mailto:19623160@std.stei.itb.ac.id'>19623160@std.stei.itb.ac.id</Link></p>
+        <p>LinkedIn: <Link href="https://linkedin.com/in/aria-judhistira-918892267" passHref={true}>Aria Judhistira</Link></p>
+        <p>Line ID: ariajudhistira2005</p>
       </div>
     );
   }
